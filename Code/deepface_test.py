1f7301deacd3a9f89446279e8866cb56b18feab1
florian-frey <83298480+florian-frey@users.noreply.github.com>
2022-01-03 17:36:15 +0100

    Update deepface_test.py

--- Code/deepface_test.py
+++ Code/deepface_test.py
@@ -3,16 +3,31 @@ from deepface import DeepFace
 import numpy as np  #this will be used later in the process
 from pathlib import Path
 
-#imgpath = r"C:\Users\Fred\Desktop\Facebank\pics\2204.jpg"
-#image = cv2.imread(imgpath)
-
-pathlist = Path('.\\data\\age\\10').rglob('*.png')
-pictures = np.array([])
-count = 0
+pathlist = Path('.\\data\\age\\50').rglob('*.png')
+pictures = []
 for path in pathlist:
     # because path is object not string
     path_in_str = str(path)
-    print("test")
-    np.insert(pictures,count,[path_in_str,10])
-    count = count + 1
-print(pictures)
+    pictures.append([path_in_str, 50])
+
+correct = 0
+false = 0
+no_face = 0
+
+for i in pictures:
+    image = cv2.imread(i[0])
+
+    try:
+        analyze = DeepFace.analyze(image, actions=['age'], enforce_detection=True)
+        if analyze['age'] > 40 and analyze['age'] < 60:
+            correct += 1
+        else:
+            false += 1
+    except:
+        no_face += 1
+
+    
+
+print("Correct:", correct)
+print("False:", false)
+print("No Face Found:", no_face)
